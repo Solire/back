6f74f8f182d82375c50fb6652561c5b301b55e5f
Monnot Stéphane <monnot.stephane@gmail.com>
2015-09-10 15:49:06 +0200

    Ajout de la configuration pour noty

--- public/js/modules/helper/noty.js
+++ public/js/modules/helper/noty.js
@@ -1,4 +1,4 @@
-define(['jquery', 'noty'], function ($) {
+define(['jquery', 'noty', 'modules/config/noty'], function ($) {
     return {
         run : function(wrap, response){
             noty({text: response.text, type: response.status});
