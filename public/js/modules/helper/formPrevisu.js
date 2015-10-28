6da1280f006e07b3484ccb833f9949087cb718d8
thansen <thansen@solire.fr>
2015-10-28 16:32:08 +0100

    SOLIREFW-352 previsualisation de page

---
+++ public/js/modules/helper/formPrevisu.js
@@ -0,0 +1,21 @@
+define(['jquery', 'modules/helper/wysiwyg'], function ($, wysiwyg) {
+  return {
+    run: function (wrap, response) {
+      var $form = $(wrap).parents('form').first();
+
+      $(wrap).click(function(){
+        var
+          $clone,
+          action = $('base').attr('href') + $(wrap).data('url');
+        ;
+
+        wysiwyg.save();
+        $clone = $form.clone();
+        $clone.attr('action', action);
+        $clone.attr('target', '_blank');
+        $clone.submit();
+        $clone.remove();
+      });
+    }
+  };
+});
--- public/js/modules/helper/wysiwyg.js
+++ public/js/modules/helper/wysiwyg.js
@@ -1,54 +1,63 @@
 define(['jquery', 'tinyMCE'], function ($, tinyMCE) {
-    return {
-        wysiwygs: [],
-        run: function (wrap, response) {
-            var s    = {
-                mode: 'none',
-                height: '290px',
-                entity_encoding: 'raw',
-                plugins: [
-                    'autolink link'
-                ],
-                menubar: false,
-                statusbar: false,
-                toolbar: 'insertfile undo redo | bold italic | bullist numlist | link image',
-                document_base_url: '../../../../',
-                image_list: 'back/media/autocomplete.html?tinyMCE',
-                link_list: 'sitemap.xml?visible=0&json=1&onlylink=1&tinymce=1',
-            }
+  return {
+    wysiwygs: {},
+    save: function(){
+      for (id in this.wysiwygs) {
+        this.wysiwygs[id].data('wysiwyg').save();
+      }
+    },
+    run: function (wrap, response) {
+      var s = {
+        mode: 'none',
+        height: '290px',
+        entity_encoding: 'raw',
+        plugins: [
+          'autolink link'
+        ],
+        menubar: false,
+        statusbar: false,
+        toolbar: 'insertfile undo redo | bold italic | bullist numlist | link image',
+        document_base_url: '../../../../',
+        image_list: 'back/media/autocomplete.html?tinyMCE',
+        link_list: 'sitemap.xml?visible=0&json=1&onlylink=1&tinymce=1',
+      }
 
-            tinymce.init({});
-            var edId = $('textarea', wrap).attr('id')
-            var ed   = tinyMCE.createEditor(edId, s);
-            wrap.addClass('wysiwyg-initialised')
-            wrap.data('wysiwyg', ed);
-            ed.render();
+      tinymce.init({});
+      var
+        edId = $('textarea', wrap).attr('id'),
+        ed = tinyMCE.createEditor(edId, s)
+      ;
 
-            this.wysiwygs[edId] = wrap;
+      wrap.addClass('wysiwyg-initialised')
+      wrap.data('wysiwyg', ed);
+      ed.render();
 
-            // Event click toggle wysiwyg
-            $('.switch-editor a', wrap).click(function (e) {
-                e.preventDefault();
-                var textarea = $('textarea', wrap);
-                if ($(this).hasClass('btn-default') && textarea.length > 0) {
-                    $(this).removeClass('btn-default').addClass('btn-info');
-                    $(this).siblings().removeClass('btn-info').addClass('btn-default');
-                    if (ed.isHidden()) {
-                        ed.show();
-                    } else {
-                        ed.hide();
-                    }
-                }
-            });
-        },
-        destroy: function (wrap) {
-            if (typeof wrap != 'undefined') {
-                var id = $('textarea', wrap).attr('id');
-                tinymce.remove("#" + id);
-                delete this.wysiwygs[id];
-                wrap.data('wysiwyg', null);
-                wrap.removeClass('wysiwyg-initialised')
-            }
+      this.wysiwygs[edId] = wrap;
+
+      // Event click toggle wysiwyg
+      $('.switch-editor a', wrap).click(function (e) {
+        e.preventDefault();
+        var textarea = $('textarea', wrap);
+
+        if ($(this).hasClass('btn-default') && textarea.length > 0) {
+          $(this).removeClass('btn-default').addClass('btn-info');
+          $(this).siblings().removeClass('btn-info').addClass('btn-default');
+          if (ed.isHidden()) {
+            ed.show();
+          } else {
+            ed.hide();
+          }
         }
-    };
+      });
+    },
+    destroy: function (wrap) {
+      if (typeof wrap != 'undefined') {
+        var id = $('textarea', wrap).attr('id');
+        tinymce.remove("#" + id);
+        delete this.wysiwygs[id];
+        wrap.data('wysiwyg', null);
+        wrap.removeClass('wysiwyg-initialised')
+      }
+    }
+  };
 });
