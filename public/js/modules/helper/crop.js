b4e2f03394ef87ff560b0fcc700d5b00b66c22f0
thansen <thansen@solire.fr>
2015-11-03 16:06:29 +0100

    edition de page | crop d'image

--- public/js/modules/helper/crop.js
+++ public/js/modules/helper/crop.js
@@ -1,46 +1,59 @@
 define(['jquery', 'jcrop'], function ($) {
-    return {
-        defaults: {
-            boxWidth: 800,
-            boxHeight: 600,
-            url: 'back/media/crop.html',
-            onclickCrop: '.exec-onclick-crop'
-        },
-        getOptions: function(wrap, options) {
-            options = $.extend(true, {}, this.defaults, options);
-
-            // Surcharge via les attributs data
-            var optionsFromData = $(wrap).data();
-            options = $.extend(true, {}, options, optionsFromData);
-
-            return options;
-        },
-        run: function (wrap, options) {
-            var currentModule = this;
-
-            options = this.getOptions(wrap, options);
-            $(wrap).data('helper-crop-options', options)
-            $(wrap).Jcrop(options);
-
-            $(wrap).Jcrop('api').container.on('cropmove cropend',function(e,s,c){
-                $(wrap).data('crop', c);
-            });
-
-            $(document.body).off('click', options.onclickCrop).on('click', options.onclickCrop, function() {
-                currentModule.crop(wrap, options);
-            })
-        },
-        crop: function(wrap) {
-            var options = $(wrap).data('helper-crop-options'),
-                cropData = $(wrap).data('crop'),
-                cropData = $.extend(true, {}, $(wrap).data('params'), cropData);
-
-            cropData.src = options.src;
-            cropData.dest = options.dest;
-
-            $.post(options.url, cropData, function(response) {
-                $(wrap).trigger( "croped.helper.crop", response );
-            }, 'json');
-        }
+  function cropInstance(wrap, options)
+  {
+    this.defaults = {
+      boxWidth: 800,
+      boxHeight: 600,
+      url: 'back/media/crop.html',
+      params: {}
     };
+    this.wrap = wrap;
+    this.options = {};
+    this.api = null;
+
+    this.init = function (options) {
+      var self = this, minSize, newMS;
+
+      $.extend(self.options, self.defaults, options, $(self.wrap).data());
+
+      minSize = self.options.minSize;
+      delete self.options.minSize;
+
+      $(this.wrap).Jcrop(self.options);
+      self.api = $(this.wrap).Jcrop('api');
+
+      newMS = self.api.ui.manager.core.scale({
+        w: minSize[0],
+        h: minSize[1]
+      });
+      minSize = [
+        newMS.w,
+        newMS.h
+      ];
+      self.api.setOptions({
+        minSize: minSize
+      });
+    }
+
+    this.init(options);
+  }
+
+  cropInstance.prototype.sendRequest = function (data) {
+    var
+      self = this,
+      data = data || {}
+    ;
+
+    $.extend(self.options.params, data);
+
+    $.post(self.options.url, self.options.params, function (response) {
+      $(self.wrap).trigger('croped.helper.crop', response);
+    }, 'json');
+  };
+
+  cropInstance.prototype.getApi = function () {
+    return this.api;
+  }
+
+  return cropInstance;
 });
