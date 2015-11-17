2ed5d278a00b6d2002c3137e37540d7bc4946e8b
Monnot Stéphane <monnot.stephane@gmail.com>
2015-11-17 18:46:50 +0100

    Ajout d'un prevent default sur le lien "Mot de passe perdu"

--- public/js/modules/page/signin.js
+++ public/js/modules/page/signin.js
@@ -3,7 +3,8 @@ define(['jquery', 'noty'], function ($) {
     run: function (wrap, response) {
 
       // Lien pour passer du formulaire de connexion au formulaire de mot de passe perdu
-      $(document).on('click', '.forgot-password-link, .sign-in-link', function () {
+      $(document).on('click', '.forgot-password-link, .sign-in-link', function (e) {
+        e.preventDefault();
         $('form').toggleClass('hidden');
       })
     }
