50886c6f779c3b7ff5e25777c05f9e322236bd2b
Monnot Stéphane <monnot.stephane@gmail.com>
2015-11-30 11:23:41 +0100

    Correction du controle des datepicker

--- public/js/modules/helper/datepicker.js
+++ public/js/modules/helper/datepicker.js
@@ -1,7 +1,15 @@
 define(['jquery', 'modules/config/datepicker'], function ($) {
-    return {
-        run: function (wrap, response) {
-            $(wrap).datepicker()
-        }
-    };
+  return {
+    run: function (wrap, response) {
+      $(wrap).datepicker()
+          .on('changeDate clearDate', function (e) {
+            $(this).trigger('focusout', true);
+          })
+          .bind("focusout", function (e, is_changeDate) {
+            if (!is_changeDate) {
+              e.stopPropagation();
+            }
+          });
+    }
+  };
 });
