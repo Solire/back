df1f5e60bc035c61bb1c8a8bc089ed08e1c39e34
thansen <thansen@solire.fr>
2017-03-03 14:55:42 +0100

    Debug redirection (ajout base href)

--- public/js/modules/render/aftersignin.js
+++ public/js/modules/render/aftersignin.js
@@ -2,15 +2,15 @@ define(['jquery'], function ($, helperMessage, helperDatatable) {
   return {
     run: function (wrap, response) {
       var signinForm = $('#sign-in');
-      if (response.status != 'success') {
+      if (response.status !== 'success') {
         // clear the fields to discourage brute forcing :)
 
         $('#log', signinForm).val('');
         $('#pwd', signinForm).val('');
       } else {
         $(signinForm).delay(800).queue(function () {
-          document.location.href = signinForm.attr('action');
-        })
+          document.location.href = $('base').attr('href') + signinForm.attr('action');
+        });
       }
     }
   };
